252d2e3f3ff3759ab04f3adad31ebda3b131a4e8
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2022-12-12 21:16:25 +0900

    20221212_study
    오늘첫수업
    가상머신을통해 윈도우설치 연습

---
+++ WorkSpace/NaNaWa_20221212/NaNaWa_20221212/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main()
+{
+
+	/*
+	int a = 3;	//숫자를 나타내는 변수입니다.
+	int val1, val2;
+
+	std::cout << "냐냐와 첫수업입니다." << std::endl;
+	std::cout << "제임스입니다.\n";
+	std::cout << a << std::endl;
+
+	std::cout << "숫자를 입력해주세요" << std::endl;
+	std::cin >> val1;
+	std::cout << val1 << "를 입력하였습니다";
+	*/
+
+	return 0;
+}
