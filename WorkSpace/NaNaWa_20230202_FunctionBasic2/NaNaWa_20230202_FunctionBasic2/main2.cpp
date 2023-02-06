34c722f5549d286a0cd6369eb117d92d10e72665
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2023-02-06 20:26:10 +0900

    재귀함수
    Recusion 피보나치수열,팩토리얼 예시

--- WorkSpace/NaNaWa_20230202_FunctionBasic2/NaNaWa_20230202_FunctionBasic2/main2.cpp
+++ WorkSpace/NaNaWa_20230202_FunctionBasic2/NaNaWa_20230202_FunctionBasic2/main2.cpp
@@ -19,11 +19,68 @@ int Factorial(int _Count)
 	return iValue;
 }
 
-//재귀함수란 
-//함수 안에서 자기 자신의 함수를 호출하는 방식
-//호출스택 더블클릭하면 다른 쪽도 볼수 있다 중요함
-//함수 안에서 자기함수를 계속불러오면(재귀함수) 스택의 한계치를 도달하게되는데
-//이걸 스택오버 플로우라고 한다.
+
+
+
+/*	재귀(Recursion)함수란 
+	함수 안에서 자기 자신의 함수를 호출하는 방식
+	호출스택 더블클릭 하면 다른 쪽도 볼수 있다 중요함
+	함수 안에서 자기함수를 계속 불러오면(재귀함수) 스택의 한계치를 도달하게 되는데
+	이걸 스택오버 플로우 라고 한다.
+	
+
+*/
+
+int Factorial_Re(int _Count)
+{
+	if (_Count == 1)
+	{
+		return 1;
+	}
+
+	return _Count * Factorial_Re(_Count - 1);
+}
+
+//피보나치수열
+//1 1 2 3 5 8 13 21 34 55 89
+int Fibonacci(int _Num)
+{
+	int iPreNum = 1;
+	int iPreNum2 = 1;
+
+	int iResult = 0;
+
+	if (_Num == 1 || _Num == 2)
+	{
+		return 1;
+	}
+
+	for (int i = 0; i < _Num - 2; ++i)
+	{
+		
+		iResult = iPreNum + iPreNum2;
+
+		//순서를 잘못하면 이상한 값이 들어가게 된다.
+		//iPreNum2 = iResult;
+		//iPreNum = iPreNum2;
+		iPreNum = iPreNum2;
+		iPreNum2 = iResult;
+		
+	}
+
+	return iResult;
+}
+
+int Fibonacci_Re(int _Num)
+{
+	if (_Num == 1 || _Num == 2)
+	{
+		return 1;
+	}
+
+	return Fibonacci_Re(_Num - 1) + Fibonacci_Re(_Num - 2);
+	
+}
 
 
 int main()
@@ -46,5 +103,18 @@ int main()
 
 	std::cout << "iResult = " << iResult << std::endl;
 
+	iResult = Factorial_Re(5);
+
+	std::cout << "iResult = " << iResult << std::endl;
+
+	//피보나치수열 
+	iResult = Fibonacci(8);
+	std::cout << "iResult = " << iResult << std::endl;
+
+	iResult = Fibonacci_Re(5);
+	std::cout << "iResult = " << iResult << std::endl;
+
 	return 0;
 }
+
+//배열
